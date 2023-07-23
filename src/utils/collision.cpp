3b038acea4881573d2a5cd51006a3fbd6c7c5eb5
zatkiller <gheedylan@gmail.com>
2023-07-23 19:34:55 +0800

    add collision util functions

---
+++ src/utils/collision.cpp
@@ -0,0 +1,24 @@
+#include "collision.h"
+
+#include <cmath>
+
+namespace game {
+
+float getRadius(const sf::Sprite& s) {
+  sf::Vector2f rect(s.getTextureRect().width, s.getTextureRect().height);
+  rect.x *= s.getScale().x;
+  rect.y *= s.getScale().y;
+  return (rect.x + rect.y) / 4;
+}
+
+bool circleTest(const sf::Sprite& first, const sf::Sprite& second) {
+  float radius1 = getRadius(first), radius2 = getRadius(second);
+
+  float xd = first.getPosition().x - second.getPosition().x;
+  float yd = first.getPosition().y - second.getPosition().y;
+
+  // Pythagoras
+  return std::sqrt(xd * xd + yd * yd) <= radius1 + radius2;
+}
+
+}  // namespace game
---
+++ src/utils/collision.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace game {
+
+static bool collisionTest(const sf::Sprite& first, const sf::Sprite& second);
+
+}  // namespace game
